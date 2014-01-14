6ea3220d45c120fcc2f187686b347c95e57d5a21
guns <self@sungpae.com>
2014-01-14 09:26:19 -0600

    Don't bother with defrecord and cut a few corners

--- autoload/vim_clojure_highlight.clj
+++ autoload/vim_clojure_highlight.clj
@@ -1,23 +1,20 @@
 (ns vim-clojure-highlight
   (:require [clojure.string :as string]))
 
-(defrecord NsReference [name var])
-
-(defn external-refs [ns]
+(defn- external-refs [ns]
   (->> (ns-refers ns)
        (remove #(contains? '#{clojure.core cljs.core}
-                           (ns-name (:ns (meta (peek %))))))
-       (mapv #(NsReference. (str (first %)) (peek %)))))
+                           (ns-name (:ns (meta (peek %))))))))
 
-(defn aliased-refs [ns]
+(defn- aliased-refs [ns]
   (reduce
     (fn [v [alias alias-ns]]
       (let [alias (str alias \/)]
-        (into v (mapv #(NsReference. (str alias (first %)) (peek %))
+        (into v (mapv #(vector (str alias (first %)) (peek %))
                       (ns-publics alias-ns)))))
     [] (ns-aliases ns)))
 
-(defn var-type [v]
+(defn- var-type [v]
   (let [f @v m (meta v)]
     (cond (:macro m) :macro
           (or (contains? m :arglists)
@@ -25,15 +22,15 @@
               (instance? clojure.lang.MultiFn f)) :fn
           :else :def)))
 
-(defn syntax-keyword-statements [syntax-keywords]
+(defn- syntax-keyword-statements [seq-name->var]
   (mapv
     (fn [[type sks]]
-      (let [names (string/join \space (mapv :name sks))]
+      (let [names (string/join \space (mapv first sks))]
         (case type
           :macro (str "syntax keyword clojureMacro " names)
           :fn    (str "syntax keyword clojureFunc " names)
           :def   (str "syntax keyword clojureVariable " names))))
-    (group-by (comp var-type :var) syntax-keywords)))
+    (group-by (comp var-type peek) seq-name->var)))
 
 (defn ns-syntax-command [ns]
   (->> (concat (external-refs ns)
